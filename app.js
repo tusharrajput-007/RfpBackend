0288d5e8e3a64f76282e8b3412828d7e00355c36
tusharrajput-007 <tushar.rajput@velsof.com>
2026-05-25 13:42:02 +0530

    updated cors for production

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://tushar-rfp.veldev.com'],
+    credentials: true
+}))
 
 app.use("/api", require("./routes/auth.routes"));
 app.use("/api/rfp", require("./routes/rfp.routes"));
